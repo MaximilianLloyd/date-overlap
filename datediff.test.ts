2d5eb492f38c2fabf890c593ac727d5ad2c455bd
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2024-02-06 14:25:50 +0100

    Funcioning

---
+++ datediff.test.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect } from "bun:test";
+import { checkOverlap } from "./datediff";
+
+describe("DateDiff", () => {
+  it("overlap should be true", () => {
+    const range1 = {
+      start: new Date("2020-01-01"),
+      end: new Date("2020-01-10"),
+    };
+
+    const range2 = {
+      start: new Date("2020-01-05"),
+      end: new Date("2020-01-15"),
+    };
+
+    expect(checkOverlap([range1, range2])).toBe(true);
+  });
+  
+    it("overlap should be false", () => {
+    const range1 = {
+      start: new Date("2020-01-01"),
+      end: new Date("2020-01-04"),
+    };
+
+    const range2 = {
+      start: new Date("2020-01-06"),
+      end: new Date("2020-01-15"),
+    };
+
+    expect(checkOverlap([range1, range2])).toBe(false);
+  });
+
+   it("should be interchangable", () => {
+    const range1 = {
+      start: new Date("2020-01-01"),
+      end: new Date("2020-01-10"),
+    };
+
+    const range2 = {
+      start: new Date("2020-01-05"),
+      end: new Date("2020-01-15"),
+    };
+  
+     expect(checkOverlap([range2, range1])).toBe(true);
+   });
+});
---
+++ datediff.ts
@@ -0,0 +1,13 @@
+type DateRange = {
+    start: Date;
+    end: Date;
+}
+
+const epoch = (date: Date): number => date.getTime()
+
+export function checkOverlap([r1, r2]: [DateRange, DateRange]): boolean {
+    const r1l = (epoch(r1.end) - epoch(r1.start))
+    const r2l = (epoch(r2.end) - epoch(r2.start))
+
+    return Math.abs(epoch(r2.end) - epoch(r1.start)) < Math.abs(r1l + r2l)
+}
